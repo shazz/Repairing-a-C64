d9c8aa667692ebb7bee97e7f50709ac8135c71eb
shazz <shazz.ng@gmail.com>
2018-08-04 09:42:20 -0400

    better anon

--- src/anonymize_jpeg.py
+++ src/anonymize_jpeg.py
@@ -1,6 +1,6 @@
 # Script to remove EXIF metadata from JPEG images
 #
-# Deps: pip install Image
+# Deps: pip install Pillow
 #
 
 from PIL import Image
@@ -10,17 +10,18 @@ import os, sys, glob
 os.chdir("../photos")
 files = glob.glob('./**/*.JPG', recursive=True)
 for imgfile in files:
-    print("Stripping EXIF metadata from {0}".format(imgfile))
+    print("Loading {0}".format(imgfile))
 
     image = Image.open(imgfile)
     
     if image._getexif() is not None:
         for (k,v) in image._getexif().items():
-                print("{0} = {1}".format(TAGS.get(k), v))
+                print("\t{0} = {1}".format(TAGS.get(k), v))
+        data = list(image.getdata())
+        image_stripped = Image.new(image.mode, image.size)
+        image_stripped.putdata(data)
+        image_stripped.save(imgfile)
+        print("EXIF data removed")
     else:
-        print("No EXIF metadata found !")
+        print("No EXIF metadata found, skipping")
         
-    data = list(image.getdata())
-    image_stripped = Image.new(image.mode, image.size)
-    image_stripped.putdata(data)
-    image_stripped.save(imgfile)
