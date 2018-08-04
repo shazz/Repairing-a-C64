5456148e87f09598617ed86a8c0dd6b0ecafb895
shazz <shazz.ng@gmail.com>
2018-08-04 15:21:33 -0400

    optimize LFS storage

--- src/anonymize_jpeg.py
+++ src/anonymize_jpeg.py
@@ -1,4 +1,5 @@
 # Script to remove EXIF metadata from JPEG images
+# And try to optimize size
 #
 # Deps: pip install Pillow
 #
@@ -16,12 +17,20 @@ for imgfile in files:
     
     if image._getexif() is not None:
         for (k,v) in image._getexif().items():
-                print("\t{0} = {1}".format(TAGS.get(k), v))
-        data = list(image.getdata())
-        image_stripped = Image.new(image.mode, image.size)
-        image_stripped.putdata(data)
-        image_stripped.save(imgfile)
-        print("EXIF data removed")
-    else:
-        print("No EXIF metadata found, skipping")
-        
+            print("\t{0} = {1}".format(TAGS.get(k), v))
+        print("EXIF data will be removed")
+
+    data = list(image.getdata())
+    image_stripped = Image.new(image.mode, image.size)
+    image_stripped.putdata(data)
+    image_stripped.save(imgfile, optimize=True)
+    print("Stripped image saved: {0}".format(imgfile))
+
+    width, height = image_stripped.size
+    ratio = width/height
+
+    image_stripped_small = image_stripped.resize((512,int(512/ratio)),Image.ANTIALIAS)
+    image_stripped_small.save(os.path.splitext(imgfile)[0] + "_TN.JPG",optimize=True)
+    print("Thumbnail image saved: {0}".format(os.path.splitext(imgfile)[0] + "_TN.JPG"))
+    #else:
+    #    print("No EXIF metadata found, skipping")
